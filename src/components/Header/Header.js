995784db0207e2bf063d016021075722838d30ea
Samat <47773886+SamatFontendDev@users.noreply.github.com>
2019-08-13 17:55:10 +0300

    button in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import './Header.css'
 import { AuthConsumer } from '../../contexts/Auth';
-
+import Button from '../Button/Button'; 
 
 
 class Header extends PureComponent {
@@ -13,8 +13,8 @@ class Header extends PureComponent {
             <div className="header__title">HEADER</div>
             <div className="header-menu">
               <div className="header-menu__email">{email}</div>
-              <button onClick={logout}>LogOut</button>
-           </div>
+              <Button onClick={logout} />
+            </div>
           </div>
         </header>
       );
@@ -24,8 +24,8 @@ class Header extends PureComponent {
 
 export default () => ( <AuthConsumer>
   {({ isAuthorized, email, logout }) =>
-    isAuthorized && (
-      <Header email={email} logout={logout} />
+    (
+      <Header email={isAuthorized && email} logout={logout} />
     )
   }
 </AuthConsumer>)
