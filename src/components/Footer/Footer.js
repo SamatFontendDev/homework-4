ab5e95e2082a6b574120649de259acf12c0ce7fe
Samat <47773886+SamatFontendDev@users.noreply.github.com>
2019-08-14 09:34:35 +0300

    14.08-1

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,12 +2,21 @@ import React, { PureComponent } from 'react';
 import { AuthConsumer } from '../../contexts/Auth';
 import './Footer.css';
 
+const Paragraph = () => <p className="footer-message t-footer">Вы в этой системе гость</p>
+
 class Footer extends PureComponent {
   render() {
     return (
       <footer className="footer">
         <p className="header__title section-title">footer</p>
-        <p className="footer-message t-footer">Вы в этой системе гость</p>
+        <AuthConsumer>
+        {({isAuthorized}) => 
+            (
+              isAuthorized &&
+              <Paragraph/>
+            )
+          }
+        </AuthConsumer>
       </footer>
     )
   }
@@ -15,7 +24,7 @@ class Footer extends PureComponent {
 
 export default () => (<AuthConsumer>
   {({isAuthorized}) => 
-  isAuthorized && (
+  (
     <Footer/>
   )
 }
